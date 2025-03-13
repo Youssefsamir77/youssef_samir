ffc329094b5b7fb2f3a41230b2e26dea53f7c699
Youssefsamir77 <ys7564535@gmail.com>
2025-03-13 03:39:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# task 1
+base=(float(input("enter the length of base")))
+height=(float(input("enter the length of height")))
+area_of_triangle=0.5*base*height
+print(area_of_triangle)
+# task 2
+variable_1=input("please enter your first name ")
+variable_2=input("please enter your last name")
+variable_3=variable_1
+variable_1=variable_2
+print(variable_1,variable_3)
+# task 3
+kilo_meter=float(input("please enter the distance covered in km to convert it into miles"))
+mile=0.621*kilo_meter
+print("the distance in miles =",mile)
+
